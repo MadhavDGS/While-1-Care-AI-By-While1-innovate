833f35c54c7a0158d898b777fdf046c7d71d36b9
MadhavDGS <sreemadhav.reply@gmail.com>
2025-04-13 13:32:40 +0530

    Revert to centered layout in login page

--- login.py
+++ login.py
@@ -7,7 +7,7 @@ from datetime import datetime
 st.set_page_config(
     page_title="While(1)Care AI",
     page_icon="❤️",
-    layout="wide",
+    layout="centered",
     initial_sidebar_state="collapsed"
 )
 
